487cbb17304212600284e62be9ce4992ba9177ba
Karel <karelh2207@gmail.com>
2024-11-23 14:56:02 -0300

    creating repo

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+  return HttpResponse("<h1>Home</h1>")
+
+def hello_world(request):
+  return HttpResponse("<h1>Hello, world!</h1>")
+
+def about(request):
+  return HttpResponse("<h1>About</h1>")
---
+++ readme.md
@@ -0,0 +1,19 @@
+
+pip install virtualenv
+
+.venv\Scripts\activate
+
+pip install django
+
+
+django-admin --version
+
+django-admin startproject mysite .
+
+
+python manage.py runserver
+<!-- python manage.py runserver 3000 -->
+
+python manage.py --help
+
+python manage.py startapp myapp
